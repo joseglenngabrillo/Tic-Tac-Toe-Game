8b4c9d64aaf6e69a40a2cf710446fd293caf8a0b
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-22 13:46:49 +0800

    Insert the Reset button.

--- Tic-Tac-Toe Game/Form1.Designer.cs
+++ Tic-Tac-Toe Game/Form1.Designer.cs
@@ -39,6 +39,7 @@ namespace Tic_Tac_Toe_Game
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -135,12 +136,24 @@ namespace Tic_Tac_Toe_Game
             this.button10.UseVisualStyleBackColor = false;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(147, 12);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 43);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Reset";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(394, 461);
+            this.Controls.Add(this.button11);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -171,6 +184,7 @@ namespace Tic_Tac_Toe_Game
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
     }
 }
 
