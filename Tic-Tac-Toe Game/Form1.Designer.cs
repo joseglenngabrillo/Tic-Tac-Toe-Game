1834257057b53de4719f95c884b377a6920ac050
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-22 13:09:28 +0800

    Place the second button in position.

--- Tic-Tac-Toe Game/Form1.Designer.cs
+++ Tic-Tac-Toe Game/Form1.Designer.cs
@@ -30,16 +30,26 @@ namespace Tic_Tac_Toe_Game
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(96, 119);
+            this.button1.Location = new System.Drawing.Point(100, 78);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 100);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(218, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -47,6 +57,7 @@ namespace Tic_Tac_Toe_Game
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -59,6 +70,7 @@ namespace Tic_Tac_Toe_Game
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
