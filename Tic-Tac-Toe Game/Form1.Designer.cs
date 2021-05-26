c36d73ca705024a4e10694f13746112b88b716e5
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-26 23:06:50 +0800

    Changed the Back Color of btnExit.

--- Tic-Tac-Toe Game/Form1.Designer.cs
+++ Tic-Tac-Toe Game/Form1.Designer.cs
@@ -146,7 +146,7 @@ namespace Tic_Tac_Toe_Game
             // 
             // btnExit
             // 
-            this.btnExit.BackColor = System.Drawing.SystemColors.Info;
+            this.btnExit.BackColor = System.Drawing.Color.OrangeRed;
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.Location = new System.Drawing.Point(197, 12);
             this.btnExit.Name = "btnExit";
